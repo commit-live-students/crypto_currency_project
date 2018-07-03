aee25d10a5a59a22ee1228054f006c370d224923
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-07-03 07:37:02 +0000

    Done

--- q07_make_stationary/build.py
+++ q07_make_stationary/build.py
@@ -3,6 +3,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-
+path = 'data/bitcoin.csv'
 def q07_make_stationary(path):
-    
+    df = pd.read_csv(path)
+    df.set_index('Date',inplace=True)
+    df['lClose'] = np.log(df['Close**'])
+    moving_avg = pd.rolling_mean(df['lClose'],window=12)
+    df['sClose'] = df['lClose'] - moving_avg
+    return df
+
+#q07_make_stationary(path)
