20e7b4b6245f57ffc8268b8873d95919b1bf4ea4
djocz <jocelynd789@gmail.com>
2018-07-05 12:45:23 +0000

    Done

--- q01_scrape_date/build.py
+++ q01_scrape_date/build.py
@@ -9,10 +9,8 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20170101&end=20180401"
 
 def q01_scrape_date(url):
-    
-
-
-
-
-    
-
+    df = pd.read_html(url)[0]
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values(by='Date')
+    df.to_csv('./data/bitcoin.csv')
+    return df
