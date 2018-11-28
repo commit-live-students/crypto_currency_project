ba44b185afd6f2646aacd1e93587c5c357cfba02
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-11-28 05:03:32 +0000

    Done

--- q03_lag_plots/build.py
+++ q03_lag_plots/build.py
@@ -1,12 +1,23 @@
+# %load q03_lag_plots/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import lag_plot, autocorrelation_plot
 from sklearn.model_selection import train_test_split
+plt.switch_backend('agg')
 
+path = './data/bitcoin.csv'
 
 def q03_lag_plots(path, n_lags):
+    df = pd.read_csv(path)
+    df.set_index('Date',inplace=True)
+    lag_plot(df,lag=n_lags)
+    plt.show()
     
+q03_lag_plots(path,4)
+
+
+
 
 
 
