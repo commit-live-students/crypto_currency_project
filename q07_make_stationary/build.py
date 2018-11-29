3ea17af3223689306ae0dcb7d5294b8a8d8c1762
rianboy18 <pranavr95.pn@gmail.com>
2018-11-29 13:36:44 +0000

    Done

--- q07_make_stationary/build.py
+++ q07_make_stationary/build.py
@@ -1,8 +1,23 @@
+# %load q07_make_stationary/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+path = './data/bitcoin.csv'
+df = pd.read_csv(path,sep='\t')
+df['Date'] = pd.to_datetime(df['Date'])
+df=df.set_index('Date')
+df = df.drop(['Unnamed: 0'],1)
+df.plot(['Close**'])
 
 
 def q07_make_stationary(path):
+    df['lClose'] = np.log(df['Close**'])
+    moving_avg = pd.rolling_mean(df['lClose'],window=12)
+    df['sClose']= df['lClose'] - moving_avg
+    return df
     
+    
+
+
+
--- q08_moving_average/build.py
+++ q08_moving_average/build.py
@@ -1,8 +1,25 @@
+# %load q08_moving_average/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+path = './data/bitcoin.csv'
+df = pd.read_csv(path,sep='\t')
+df=df.set_index('Date')
+df = df.drop(['Unnamed: 0'],1)
 
 
 def q08_moving_average(path):
-    
+    df['20d'] = pd.rolling_mean(df['Close**'],window=1)
+    df['100d'] = pd.rolling_mean(df['Close**'],window = 1)
+    df.plot(['Close**','20d','100d'])
+    plt.show()
+    return df
+
+
+c = q08_moving_average(path)
+
+c
+
+
