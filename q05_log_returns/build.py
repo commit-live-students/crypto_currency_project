d1e925be4c361598b77e41f0f798fd32b01f9b86
rianboy18 <pranavr95.pn@gmail.com>
2018-11-29 12:48:07 +0000

    Done

--- q05_log_returns/build.py
+++ q05_log_returns/build.py
@@ -1,10 +1,21 @@
+# %load q05_log_returns/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-
+plt.switch_backend('agg')
+path = './data/bitcoin.csv'
+df = pd.read_csv(path,sep='\t')
+df['Date'] = pd.to_datetime(df['Date'])
+df=df.set_index('Date')
+df = df.drop(['Unnamed: 0'],1)
 def q05_log_returns(path):
+    df['Close**'] = np.log(df['Close**'])
+    df.plot(['Close**'])
+    plt.show()
+    return df
     
-    
+
+
+
 
--- q06_stationarity/build.py
+++ q06_stationarity/build.py
@@ -1,11 +1,24 @@
+# %load q06_stationarity/build.py
+import warnings
 import pandas as pd
 import numpy as np
 import sys, os
 from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import sys, os
 
-
-#path = 'data/bitcoin.csv'
-
+path = 'data/bitcoin.csv'
+df = pd.read_csv(path,sep='\t')
+df=df.set_index('Date')
+df = df.drop(['Unnamed: 0'],1)
 
 def q06_stationarity(path):
-    
+    res = adfuller(df['Close**'])
+    stat = res[0]
+    p_value = res[1]
+    return stat,p_value
+c = q06_stationarity(path)
+
+
+
